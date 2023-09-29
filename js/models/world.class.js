59c7681a7aba76e4ab8b7a89f721d81c51a7296f
Toby-22 <112898220+Toby-22@users.noreply.github.com>
2023-09-29 20:41:27 +0200

    display character, chickens and clouds

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -5,14 +5,31 @@ class World{
         new Chicken(),
         new Chicken(),
     ];
+    clouds = [
+        new Cloud(),
+    ];
     ctx;
+    canvas;
 
     constructor(canvas){
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw(){
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
+        this.ctx.clearRect(0 ,0 , this.canvas.width, this.canvas.height);
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.enemies.forEach(chicken => {
+            this.ctx.drawImage(chicken.img, chicken.x, chicken.y, chicken.width, chicken.height)            
+        });
+        this.clouds.forEach(cloud => {
+            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
+        })
+        
+        self = this;
+        requestAnimationFrame(function(){
+            self.draw();
+        });
     }
 }
