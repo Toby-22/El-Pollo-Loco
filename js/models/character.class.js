de2069d6d6fee629162c4e887046740f980170ae
Tobias <112898220+Toby-22@users.noreply.github.com>
2023-10-08 19:26:39 +0200

    input endboss

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -12,6 +12,7 @@ class Character extends MovableObject {
     "img/2_character_pepe/2_walk/W-26.png",
   ];
   world;
+  walking_sound = new Audio('sounds/walking.mp3');
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
@@ -21,28 +22,26 @@ class Character extends MovableObject {
 
   animate() {
     setInterval(() => {
-      if (this.world.keyboard.RIGHT) {
+      this.walking_sound.pause();
+      if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
         this.x += this.speed;
+        this.otherDirection = false;
+        this.walking_sound.play();
       }
 
-      if (this.world.keyboard.LEFT) {
+      if (this.world.keyboard.LEFT && this.x > 0) {
         this.x -= this.speed;
+        this.otherDirection = true;
+        this.walking_sound.play();
       }
+      this.world.camera_x = -this.x + 100;
     }, 1000/60);
     setInterval(() => {
       if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
         // walk animation
-        let i = this.currentImage % this.IMAGES_WALKING.length;
-        let path = this.IMAGES_WALKING[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
+        this.playAnimation(this.IMAGES_WALKING);
       }
     }, 40);
   }
-
-  moveRight() {
-    console.lot("moving right");
-  }
-
-  jump() {}
 }
+  
