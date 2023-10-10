d0da69a0c6f3fa5c5eac77c719c1d75f6589dbbf
Tobias <112898220+Toby-22@users.noreply.github.com>
2023-10-10 19:56:08 +0200

    character can jump and run, and collisions with enemy will detect

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -1,37 +1,89 @@
 class MovableObject {
-    x = 120;
-    y = 340;
-    width = 100;
-    height = 90;
-    img;
-    imageCache = {};
-    currentImage = 0;
-    speed = 0.15;
-    otherDirection = false;
+  x = 120;
+  y = 340;
+  width = 100;
+  height = 90;
+  img;
+  imageCache = {};
+  currentImage = 0;
+  speed = 0.15;
+  otherDirection = false;
+  speedY = 0;
+  acceleration = 2.5;
+  energy = 100;
 
-    loadImage(path){
-        this.img = new Image();
-        this.img.src = path;
-    }
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
 
-    loadImages(arr){
-        arr.forEach(path => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
 
-    playAnimation(images){
-        let i = this.currentImage % this.IMAGES_WALKING.length;
-        let path = images[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
-    }
+  playAnimation(images) {
+    let i = this.currentImage % this.IMAGES_WALKING.length;
+    let path = images[i];
+    this.img = this.imageCache[path];
+    this.currentImage++;
+  }
 
-    moveLeft(){
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60)
+  moveLeft() {
+    this.x -= this.speed;
+  }
+
+  moveRight() {
+    this.x += this.speed;
+    this.otherDirection = false;
+  }
+
+  applyGravity() {
+    setInterval(() => {
+      if (this.isAboveGround() || this.speedY > 0) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
+      }
+    }, 1000 / 25);
+  }
+
+  isAboveGround() {
+    return this.y < 180;
+  }
+
+  jump() {
+    this.speedY = 30;
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    if (this instanceof Character || this instanceof Chicken) {
+      ctx.beginPath();
+      ctx.lineWidth = "10";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
     }
+  }
+
+  isColliding(mo){
+    return this.x + this.width > mo.x &&
+    this.y + this.height > mo.y &&
+    this.x < mo.x &&
+    this.y < mo.y + mo.height
+  }
+
+  isColliding2 (obj) {
+        return  (this.X + this.width) >= obj.X && this.X <= (obj.X + obj.width) && 
+                (this.Y + this.offsetY + this.height) >= obj.Y &&
+                (this.Y + this.offsetY) <= (obj.Y + obj.height) && 
+              obj.onCollisionCourse; // Optional: hiermit könnten wir schauen, ob ein Objekt sich in die richtige Richtung bewegt. Nur dann kollidieren wir. Nützlich bei Gegenständen, auf denen man stehen kann.
+
+}
 }
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -12,10 +12,22 @@ class World {
     this.keyboard = keyboard;
     this.draw();
     this.setWorld();
+    this.checkCollisions();
   }
 
   setWorld() {
     this.character.world = this;
+  }
+
+  checkCollisions(){
+    setInterval(() => {
+      this.level.enemies.forEach((enemy) => {
+        if(this.character.isColliding(enemy)){
+          this.character.energy -= 5;
+          console.log('Collision with Character! Energy Character is:', this.character.energy);
+        };
+      })
+    }, 100);
   }
 
   draw() {
@@ -39,23 +51,26 @@ class World {
     });
   }
 
-  addToMap(movableObject) {
-    if (movableObject.otherDirection) {
-      this.ctx.save();
-      this.ctx.translate(movableObject.width, 0);
+  addToMap(mo) {
+    if (mo.otherDirection) {
+      this.flipImage(mo);
+    }
+    mo.draw(this.ctx);
+    mo.drawFrame(this.ctx);
+    if (mo.otherDirection) {
+      this.flipImageBack(mo);
+    }
+  }
+
+  flipImage(mo){
+    this.ctx.save();
+      this.ctx.translate(mo.width, 0);
       this.ctx.scale(-1, 1);
-      movableObject.x = movableObject.x * -1;
-    }
-    this.ctx.drawImage(
-      movableObject.img,
-      movableObject.x,
-      movableObject.y,
-      movableObject.width,
-      movableObject.height
-    );
-    if (movableObject.otherDirection) {
-      movableObject.x = movableObject.x * -1;
-      this.ctx.restore();
-    }
+      mo.x = mo.x * -1;
+  }
+
+  flipImageBack(mo){
+    mo.x = mo.x * -1;
+    this.ctx.restore();
   }
 }
